dfb96a18d15c3faebcb450dacd5ba2b50d6a30fd
zankoAn <zankoan4@gmail.com>
2024-03-01 22:34:28 +0330

    feat: Implement message loading and dumping functionality

---
+++ app/database/utils/dump_data.py
@@ -0,0 +1,35 @@
+import json
+from pathlib import Path
+
+from app.database.database import Session
+from app.database.models import Message
+
+
+class DumpMsg:
+    def __init__(self) -> None:
+        self.path = self.get_file_path()
+        self.session = Session()
+
+    def get_file_path(self):
+        base_dir = Path(__file__).resolve().parent.parent.parent
+        return base_dir / "fixtures/messages.json"
+
+    def read_db_msg(self):
+        messages = self.session.query(Message).all()
+        return [
+            {
+                "text": msg.text,
+                "key": msg.key,
+                "keys": msg.keys,
+                "keys_per_row": msg.keys_per_row
+            } for msg in messages
+        ]
+
+    def dump_msg_to_file(self, data):
+        with open(self.path, "w") as json_file:
+            json.dump(data, json_file, indent=2)
+
+    def run(self):
+        data = self.read_db_msg()
+        self.dump_msg_to_file(data)
+        self.session.close()
---
+++ app/database/utils/load_data.py
@@ -0,0 +1,45 @@
+import json
+import os
+from pathlib import Path
+
+from app.database.database import Session
+from app.database.models import Message
+
+
+class LoadMsg:
+    def __init__(self) -> None:
+        self.path = self.get_file_path()
+        self.session = Session()
+
+    def get_file_path(self):
+        base_dir = Path(__file__).resolve().parent.parent.parent
+        return base_dir / "fixtures/messages.json"
+
+    def is_valid_path(self):
+        return os.path.exists(self.path)
+
+    def read_file(self):
+        with open(self.path, "r") as json_file:
+            return json.load(json_file)
+
+    def create_msg(self, data):
+        return Message(
+            text=data["text"],
+            key=data["key"],
+            keys=data["keys"],
+            keys_per_row=data["keys_per_row"]
+        )
+
+    def load_msg(self, messages):
+        for data in messages:
+            msg = self.create_msg(data)
+            self.session.add(msg)
+            self.session.commit()
+
+    def run(self):
+        if not self.is_valid_path():
+            exit(f"File: {self.path} not found")
+
+        msgs = self.read_file()
+        self.load_msg(msgs)
+        self.session.close()
