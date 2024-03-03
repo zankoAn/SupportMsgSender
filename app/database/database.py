ad13ffbe0ac9b43ed20eb7b72dfa2de28530d331
zankoAn <zankoan4@gmail.com>
2024-03-03 17:03:17 +0330

    refactor: read config from env file

--- app/database/database.py
+++ app/database/database.py
@@ -1,7 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from app.utils.load_env import config
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./db.sqlite3"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    url=config.DB_URL,
+    pool_size=int(config.POOL_SIZE),
+    max_overflow=int(config.MAX_OVERFLOW),
+    pool_pre_ping=bool(config.POOL_PRE_PING),
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
